60f8992db4d3e3f1f2a968cef5a787d20fe1d596
LuizFeller <luiz.feller@terra.com.br>
2023-05-19 14:09:49 -0300

    Inserção do const id e correção do post em Edit.vue

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -6,8 +6,13 @@ export default {
     posts: Array,
   },
   data() {
+    //acessa o parâmetro id uma única vez
+    const id = this.$route.params.id;
+
     return {
-      post: this.posts[this.$route.params.id],
+      //expondo a variável id para ser acessível no template
+      id: id,
+      post: this.posts[id],
     };
   },
   components: {
@@ -17,5 +22,5 @@ export default {
 </script>
 
 <template>
-  <PostForm :post="post" />
+  <PostForm :post="post" :id="id" />
 </template>
